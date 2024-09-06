48f99a3b0c466ce2a89eab42be884c0ef3b33fe6
brunel.lee <brunel.lee@kakaohealthcare.com>
2024-09-06 12:20:57 +0900

    FIX ssh keys on github

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World! its your docker container with auto push deploy';
+    return 'Hello World! its your docker container with auto push deploy2';
   }
 }
