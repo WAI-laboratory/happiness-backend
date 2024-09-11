348edfda356621816dd2411d663e626f706bd7de
brunel.lee <brunel.lee@kakaohealthcare.com>
2024-09-11 17:47:09 +0900

    REmove jappiness prefix

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('happiness');
+  // app.setGlobalPrefix('happiness');
   
   await app.listen(3010);
 }
