b2ee536a014672d9cdf0760f5ad9796806cab19b
brunel.lee <brunel.lee@kakaohealthcare.com>
2024-09-11 16:38:41 +0900

    Add set Global Prefix happiness

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3010, '0.0.0.0');
+  app.setGlobalPrefix('happiness');
+  
+  await app.listen(3010);
 }
 bootstrap();
