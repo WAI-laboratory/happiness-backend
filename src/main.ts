40376503d8241da6440edbea7d0e6a933cc0eb52
brunel.lee <brunel.lee@kakaohealthcare.com>
2024-09-12 16:38:41 +0900

    Add CORS

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // app.setGlobalPrefix('happiness');
+  app.enableCors({
+    origin: '*', // Allow requests from all origins, you can restrict to specific origins if needed
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type, Accept',
+  });
   
   await app.listen(3010);
 }
