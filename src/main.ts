b66f9565aa6474d65a29b17977a2b187d2c4d089
Yongjun Lee <stareta1202@gmail.com>
2025-10-13 17:50:34 +0900

    Remove Global prefix

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  // app.setGlobalPrefix('happiness');
+
   app.enableCors({
     origin: '*', // Allow requests from all origins, you can restrict to specific origins if needed
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
